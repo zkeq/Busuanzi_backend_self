d36485966a78031788ec7725d8def109c83a285f
Zkeq <learnonly@learnonly.xyz>
2022-04-12 17:52:15 +0800

    上传依赖

--- get_before_data.py
+++ get_before_data.py
@@ -5,21 +5,21 @@ import time
 
 
 def get_before_data(host):
-  url = "https://busuanzi.ibruce.info/busuanzi?jsonpCallback=BusuanziCallback_777487655111"
-  payload={}
-  headers = {
-    'Referer': "https://" + host,
-    'Cookie': 'busuanziId=89D15D1F66D2494F91FB315545BF9C2A'
-  }
-  response = None
-  while not response:
-    response = requests.request("GET", url, headers=headers, data=payload)
-    print("遇到错误，正在重试")
-    time.sleep(1)
-  str_2_dict = eval(response.text[34:][:-13])
-  site_uv = str_2_dict["site_uv"]
-  site_pu = str_2_dict["site_pv"]
-  db[host] = {"pv": 0, "data": {"/": 0}}
-  db[host]["pv"] = site_pu
-  db[host]["uv"] = site_uv
-  print("写入新网址数据成功")
+    url = "https://busuanzi.ibruce.info/busuanzi?jsonpCallback=BusuanziCallback_777487655111"
+    payload = {}
+    headers = {
+        'Referer': "https://" + host,
+        'Cookie': 'busuanziId=89D15D1F66D2494F91FB315545BF9C2A'
+    }
+    response = None
+    while not response:
+        response = requests.request("GET", url, headers=headers, data=payload)
+        print("遇到错误，正在重试")
+        time.sleep(1)
+    str_2_dict = eval(response.text[34:][:-13])
+    site_uv = str_2_dict["site_uv"]
+    site_pu = str_2_dict["site_pv"]
+    db[host] = {"pv": 0, "data": {"/": 0}}
+    db[host]["pv"] = site_pu
+    db[host]["uv"] = site_uv
+    print("写入新网址数据成功")
--- pv.py
+++ pv.py
@@ -1,13 +1,14 @@
 from replit import db
 
+
 def pv(host, path):
-  if not db.get(host):
-    db[host] = {"pv": 0, "data": {"/": 0}}
-  db[host]["pv"] += 1
-  print(db[host])
-  if not db[host]["data"].get(path):
-    db[host]["data"][path] = 0
-  db[host]["data"][path] += 1
-  page_pv = db[host]["data"][path]
-  site_pv = db[host]["pv"]
-  return page_pv,site_pv
+    if not db.get(host):
+        db[host] = {"pv": 0, "data": {"/": 0}}
+    db[host]["pv"] += 1
+    print(db[host])
+    if not db[host]["data"].get(path):
+        db[host]["data"][path] = 0
+    db[host]["data"][path] += 1
+    page_pv = db[host]["data"][path]
+    site_pv = db[host]["pv"]
+    return page_pv, site_pv
