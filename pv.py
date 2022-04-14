d70113dad633e6989976be1de19253bd4d232d8b
Zkeq <learnonly@learnonly.xyz>
2022-04-14 09:51:01 +0800

    [优化]: 优化 `pv` 获取逻辑，减少读数据库次数

--- pv.py
+++ pv.py
@@ -2,13 +2,23 @@ from replit import db
 
 
 def pv(host, path):
-    if not db.get(host):
-        db[host] = {"pv": 0, "data": {"/": 0}}
-    db[host]["pv"] += 1
-    try:
-      db[host]["data"][path] += 1
+    try: 
+        pv_data = db[host]
+        print("数据库拿到pv")
     except:
-      db[host]["data"][path] = 0
-    page_pv = db[host]["data"][path]
-    site_pv = db[host]["pv"]
+        pv_data  = {"pv": 0, "data": {"/": 0}}
+        db[host] = pv_data
+    pv_data["pv"] += 1
+    print("全站pv成功加1")
+    try:
+      pv_data["data"][path] += 1
+      print("单页pv成功加1")
+    except:
+      pv_data["data"][path] = 0
+    page_pv = pv_data["data"][path]
+    print("单页pv", page_pv)
+    site_pv = pv_data["pv"]
+    print("全站pv", site_pv)
+    db[host] = pv_data
+    print("放回数据库")
     return page_pv, site_pv
